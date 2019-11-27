7f907adb90d45e51c8c2df6f22496a263eb52336
HissZxy <956527824@qq.com>
2019-11-27 12:39:01 +0800

    Million

---
+++ ScannerKeyBoardTest.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class ScannerKeyBoardTest {
+    public static void main(String[] args) {
+        System.out.println("请输入若干单词，以空格作为分隔");
+        Scanner sc = new Scanner(System.in);
+        while (sc.hasNext()) {
+            System.out.println("键盘输入的内容是："
+
+                    + sc.next());
+        }
+        System.out.println("执行吗");
+    }
+}
